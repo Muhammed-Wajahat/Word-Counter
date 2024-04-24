88c8f4c4ab4f1d4390910fc7579702885f515be8
Wajahat Ali <wajahatalisadique@gmail.com>
2024-04-24 09:04:16 -0700

    New Project

---
+++ app.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from  "inquirer"
+console.log("Wellcome to Word Counter");
+
+let answer = await inquirer.prompt([{
+    name: "sentances",
+    type:'input',
+    message:"Enter a Sentanses for find a word:"
+}])
+let word = answer.sentances.split(" ")
+console.log(`your Sentanses word is: ${word.length}`);
+
+
